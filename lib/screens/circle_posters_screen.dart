1e67a72e3c61ba7c83429f6f5ac01230d64d54f4
壹零 <guchengxi1994@qq.com>
2024-09-24 17:54:54 +0800

    feat: update list

--- lib/screens/circle_posters_screen.dart
+++ lib/screens/circle_posters_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../components/text_input/rich_text_input_field.dart';
 
@@ -14,6 +15,37 @@ class CirclePostersScreen extends StatefulWidget {
 class _CirclePostersScreenState extends State<CirclePostersScreen> {
   bool showBottom = false;
   bool expanded = false;
+  double height = 200;
+  late final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    // 监听滚动事件
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        if (height == 200) {
+          setState(() {
+            height = 400;
+          });
+        }
+      } else if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        if (height == 400) {
+          setState(() {
+            height = 200;
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +80,38 @@ class _CirclePostersScreenState extends State<CirclePostersScreen> {
                 });
               }
             },
-            child: Container(
-              color: Colors.amber,
-            ),
+            child: NestedScrollView(
+                floatHeaderSlivers: true,
+                headerSliverBuilder:
+                    (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    SliverAppBar(
+                      backgroundColor: Colors.white,
+                      title: const Text('Floating Nested SliverAppBar'),
+                      floating: true,
+                      expandedHeight: height,
+                      forceElevated: innerBoxIsScrolled,
+                    ),
+                  ];
+                },
+                body: Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  decoration: const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(15),
+                          right: Radius.circular(15))),
+                  child: ListView.builder(
+                      controller: _scrollController,
+                      padding: const EdgeInsets.all(8),
+                      itemCount: 30,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: 50,
+                          child: Center(child: Text('Item $index')),
+                        );
+                      }),
+                )),
           ),
           if (showBottom)
             Positioned(
